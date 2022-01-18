fa1ab6bce82ac547dad1ecbcc0757d9b9aac99f8
msjones3 <cheryl@realsocial.com.au>
2022-01-19 06:00:12 +1000

    install postcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
 module.exports = {
+  purge: [], // purge involves removing unused styles
+  darkMode: false, // or 'media' or 'class'
   content: ["./src/**/*.{html,js}"],
   theme: {
+    extend: {},
+  },
+  variants: {
     extend: {},
   },
   plugins: [],
